b570171ec0f252b097ca53b2a82b3dbd8fcc53fa
tuongpixta <tuong.nguyendinh@pixta.co.jp>
2024-07-26 21:16:16 +0700

    format code

--- src/components/ChatCompletions/Message/index.jsx
+++ src/components/ChatCompletions/Message/index.jsx
@@ -1,9 +1,22 @@
 import clsx from "clsx";
 
-
-const Message = ({ role, content }) => <div className={clsx("flex gap-4", role === "user" ? "flex-row-reverse" : "")}>
-    <img className="w-12 h-12 rounded-full" src={role === "user" ? "user.svg" : "assistant.svg"} />
-    <div className={clsx("rounded-xl p-3", role === "user" ? "bg-gray-200" : "bg-white")}>{content}</div>
-</div>;
+const Message = ({ role, content }) => (
+  <div
+    className={clsx("flex gap-4", role === "user" ? "flex-row-reverse" : "")}
+  >
+    <img
+      className="w-12 h-12 rounded-full"
+      src={role === "user" ? "user.svg" : "assistant.svg"}
+    />
+    <div
+      className={clsx(
+        "rounded-xl p-3",
+        role === "user" ? "bg-gray-200" : "bg-white",
+      )}
+    >
+      {content}
+    </div>
+  </div>
+);
 
 export default Message;
