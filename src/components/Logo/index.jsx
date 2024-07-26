62749f28e1bb4d18dab2729403cefaa228fe4585
tuongpixta <tuong.nguyendinh@pixta.co.jp>
2024-07-26 23:45:22 +0700

    rename

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,7 +1,7 @@
 import clsx from "clsx";
 
 const Logo = ({ className }) => (
-  <img className={clsx(className, "")} src="logo.svg" alt="NicheTensor" />
+  <img className={clsx(className, "")} src="niche-tensor.svg" alt="NicheTensor" />
 );
 
 export default Logo;
