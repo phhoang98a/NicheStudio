195fee63f4fd3f5035cd432c2d3f976a89de642b
tuongpixta <tuong.nguyendinh@pixta.co.jp>
2024-08-23 23:09:33 +0700

    test iframe

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+export default function TestPage() {
+  return (
+    <div className="h-[100vh] max-h-[100dvh]">
+      <iframe
+        width="100%"
+        height="100%"
+        src="https://studio.nichetensor.com/"
+      />
+    </div>
+  );
+}
